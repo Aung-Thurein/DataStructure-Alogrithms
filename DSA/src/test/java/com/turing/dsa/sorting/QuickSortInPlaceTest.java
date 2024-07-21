2b0ae1e2decee5a5bf96ee7737c694b540447add
aungthureintun <aungthurein163@gmail.com>
2024-07-21 23:42:17 +0630

    adding QuickSortInPlace

--- DSA/src/test/java/com/turing/dsa/sorting/QuickSortInPlaceTest.java
+++ DSA/src/test/java/com/turing/dsa/sorting/QuickSortInPlaceTest.java
@@ -4,12 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class QuickSortTest {
-
+public class QuickSortInPlaceTest {
+	
+	Sortable algo = new QuickSortInPlace();
 	@Test
 	public void QuickSortBaseCase()
 	{
-		Sortable algo = new QuickSortV1();
 		int arr [] = new int [] {8,3};
 		Util.print("Array", arr);
 		algo.sort(arr);
@@ -22,8 +22,7 @@ public class QuickSortTest {
 	@Test
 	public void QuickSortBaseCaseAnotherWord()
 	{
-		Sortable algo = new QuickSortV1();
-		int arr [] = new int [] {8,3,1};
+		int arr [] = new int [] {3,8};
 		Util.print("Array", arr);
 		algo.sort(arr);
 		Util.print("Sorted", arr);
@@ -34,7 +33,6 @@ public class QuickSortTest {
 	@Test
 	public void QuickSortManyCase()
 	{
-		Sortable algo = new QuickSortV1();
 		int arr [] = new int [] {51,2,32,43,9,3,1};
 		Util.print("Array", arr);
 		algo.sort(arr);
@@ -43,5 +41,5 @@ public class QuickSortTest {
 		assertEquals(true,Util.isSorted(arr));
 
 	}
-	
+
 }
