321722a4ebb533b8ac3f9cf4e0e76ef871e25d1e
aungthureintun <aungthurein163@gmail.com>
2024-10-20 22:25:19 +0630

    update graph DFS

---
+++ DSA/src/main/java/com/turing/dsa/adt/graph/Vertex.java
@@ -0,0 +1,13 @@
+package com.turing.dsa.adt.graph;
+
+public class Vertex {
+
+	String label;
+	int index;
+	boolean visited = false;
+	
+	public Vertex(String label)
+	{
+		this.label = label;
+	}
+}
